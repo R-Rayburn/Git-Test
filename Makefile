752af09d72484da882d28e25554f924a9fd625f5
R-Rayburn <robcrayb@ut.utm.edu>
2016-08-30 21:30:03 -0500

    Adding my first files to the project.

---
+++ Makefile
@@ -0,0 +1,7 @@
+#!/usr/bin
+Default: main.o
+	g++ main.o -o main
+
+Alt: main.cpp
+	g++ -c main.cpp
+
---
+++ main.cpp
@@ -0,0 +1,13 @@
+//Name: Robert Rayburn
+//Date: 9-1-16
+//Class: CSCI 352
+//Description: Hello World example.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Hello World!" << endl;
+
+  return 0;
+}
