16acabee753320a94858ca42288ebefb07a0e516
R-Rayburn <robcrayb@ut.utm.edu>
2016-08-30 22:02:20 -0500

    Updated main.cpp that has user input.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,41 @@
 //Name: Robert Rayburn
 //Date: 9-1-16
 //Class: CSCI 352
-//Description: Hello World example.
+//Description: Hello World example with user input.
 
 #include <iostream>
 using namespace std;
 
+int sum(int n);
+/*
+** Description: Caluclates the sum from 1 to the integer n.
+** Input: The integer that is the end bound for the sum.
+** Return: Sum of 1 to n.
+*/
+
 int main(){
+  //Asks the user for integer input.
+  int n;
+  cout << "Give me a number: " << endl;
+  cin >> n;
+
+  //Prints Hello World!
   cout << "Hello World!" << endl;
 
+  //Prints the sum of 1 to n.
+  cout << sum(n) << " is the sum from 1 to " << n  << endl;
+  
   return 0;
+}
+
+
+int sum(int n){
+
+  int x = 0;
+
+  for(int i = 1; i < n + 1; i++){
+    x+=i;
+  }
+
+  return x;
 }
